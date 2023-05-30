3f3cc074dabd50f75c65795ba7e9596da6c595ab
Vladislav Borodin <vladboro@outlook.com>
2023-05-30 16:00:23 +0300

    Intermediate commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (postion) => {
+        console.log(postion);
+        this.setState({ lat: postion.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Latitude: {this.state.lat}</div>
+        <SeasonDisplay />
+      </div>
+    );
+  }
 }
 
 export default App;
