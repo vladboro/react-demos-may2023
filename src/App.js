91d2f2dfb4e59cb499db1b3305f069212e5bd655
Vladislav Borodin <vladboro@outlook.com>
2023-05-31 10:34:18 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
+import "./App.css"
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
   }
 
   componentDidMount() {
@@ -12,15 +13,25 @@ class App extends React.Component {
         console.log(postion);
         this.setState({ lat: postion.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        console.log(err);
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
+    const resultOk = this.state.lat && <div>Latitude: {this.state.lat}</div>
+    const resultError = this.state.errorMessage && <div>Error: {this.state.errorMessage}</div>
+    const resultLoading = (this.state.lat != null || this.state.errorMessage !== "" || (
+        <div>Loading...</div>
+      ))
     return (
       <div>
-        <div>Latitude: {this.state.lat}</div>
-        <SeasonDisplay />
+        {resultOk}
+        {resultError}
+        {resultLoading}
+        {this.state.lat && <SeasonDisplay lat={this.state.lat} />}
       </div>
     );
   }
